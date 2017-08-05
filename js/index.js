873852d25bf29adda473a637e2a080bf81db2f70
Shadman Chowdhury <chowdhuryshadman0@gmail.com>
2017-08-05 08:34:53 -0700

    Start

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".button-collapse").sideNav();
+    $('.slider').slider({height:innerHeight,indicators:false});
+    $('.slied').paroller();
+    setTimeout(function(){
+        $(".flyintext").removeClass("hidden");
+    },500)
+})
